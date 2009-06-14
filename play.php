3f2062ba3afc541764281c3f8925700cddbe69e8
liruqi <liruqi@44122350-53e9-11de-83c8-e5b33934001f>
2009-06-14 02:48:41 +0000

    rm old tables

--- play.php
+++ play.php
@@ -1,43 +1,89 @@
-﻿<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<title>test</title>
-	</head>
-	<body>
-		<div>
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <title>test</title>
+  <style type="text/css">
+#movie_info {
+  position: absolute;
+  margin-left: 500px;
+  width: 50%;
+}
+#player {
+  position: absolute;
+  border: 1px solid;
+}
+#comments {
+  position: absolute;
+  margin-top: 380px;
+  width: 98%;
+  border: 2px solid #cccccc;
+}
+  </style>
+</head>
+
 <?php
+$ip = $_SERVER["SERVER_NAME"];
 $id = $_GET["id"];
-echo "http://192.168.0.116/cgi-bin/get-flv?id=$id<br>";
-?>
-		</div>
-		<object type="application/x-shockwave-flash" data="player.swf" width="650" height="500" id="vcastr3">
-			<param name="movie" value="player.swf"/> 
-			<param name="allowFullScreen" value="true" />
-			<param name="FlashVars" value="xml=
-				<vcastr>
-					<channel>
-						<item>
-<?php
-echo "<source>http://192.168.0.116/cgi-bin/get-flv?id=$id</source>";
+
+$db = mysql_connect("localhost", "hhac", "iamharmless");
+if(!$db) {
+  die("Could not connect: " . mysql_error());
+}
+$dbname = "hhac";
+mysql_select_db($dbname, $db) or die("Cannot switch to $dbname");
+
+$query_flv = "select * from videos where id=$id";
+$res = mysql_query($query_flv, $db) or die("Query error" . mysql_error());
+$movie = mysql_fetch_array($res, MYSQL_ASSOC);
+$title = $movie["title"];
+$tags = $movie["tags"];
+$description = $movie["description"];
+$owner = $movie["owner"];
+
+$query_user = "select name from users where id='$owner'";
+$res_user = mysql_query($query_user, $db) or die("Query error" . mysql_error());
+$user = mysql_fetch_array($res_user, MYSQL_ASSOC);
+$user_name = $user["name"];
+
+mysql_close($db);
 ?>
 
-							<duration></duration>
-							<title></title>
-						</item>
-					</channel>
-					<config>
-					</config>
-					<plugIns> <!--
-						<logoPlugIn>
-							<url>logoPlugIn.swf</url>
-							<logoText>www.ruochi.com</logoText>
-							<logoTextAlpha>0.75</logoTextAlpha>
-							<logoTextFontSize>30</logoTextFontSize>
-							<logoTextLink>http://www.ruochigroup.com</logoTextLink>
-							<logoTextColor>0xffffff</logoTextColor>
-							<textMargin>20 20 auto auto</textMargin>
-						</logoPlugIn> -->
-					</plugIns>
-				</vcastr>"/>
-		</object>
-	</body>
-</html>
+
+<body>
+    <div id="play_and_info">
+
+    <div id="movie_info">
+      <span style="font-weight:bold"><?php echo $title ?></span><br/>
+      Owner: <?php echo $user_name ?><br/>
+      Tags: <?php echo $tags ?><br/>
+      Description: <?php echo $description ?><br/>
+    </div>
+
+
+    <div id="player">
+    <object type="application/x-shockwave-flash" data="player.swf" width="480" height="360" id="vcastr3">
+      <param name="movie" value="player.swf"/> 
+      <param name="allowFullScreen" value="true" />
+      <param name="FlashVars" value="xml=
+        <vcastr>
+          <channel>
+            <item>
+              <source><?php echo "http://$ip/cgi-bin/get-flv?id=$id" ?></source>
+              <duration></duration>
+              <title></title>
+            </item>
+          </channel>
+          <config>
+          </config>
+          <plugIns>
+          </plugIns>
+        </vcastr>"/>
+    </object>
+    </div>
+  </div>
+
+  <div id="comments">
+    <p>Comments</p>
+  </div>
+</body></html>
