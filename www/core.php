7e2c43005227ab04f0b1835e708e60b56777102a
liruqi <liruqi@44122350-53e9-11de-83c8-e5b33934001f>
2009-06-20 14:48:59 +0000

    update

---
+++ www/core.php
@@ -0,0 +1,46 @@
+<?php
+global $db_user;
+global $db_pass;
+global $usr_row;
+function authenticate()
+{
+    $db_user =  "hhac";
+    $db_pass =  "iamharmless";
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $name = addslashes($_POST["name"]);
+        $pass = addslashes($_POST["pass"]);
+        $db = mysql_connect("localhost", $db_user, $db_pass);
+        mysql_select_db("hhac", $db);
+
+        $sql = "SELECT * FROM users WHERE name='$name' and password='$pass';";
+        //echo "$sql<br>";
+        $res = mysql_query($sql, $db);
+        if($res && $usr_row = mysql_fetch_array($res, MYSQL_ASSOC))
+        {
+            //var_dump ($usr_row);
+            return $usr_row;
+        }
+        header("HTTP/1.1 403 Forbidden");
+        echo "Login failed";
+        return false;
+    }
+    else 
+    {
+        header("HTTP/1.1 404 Not Found");
+        echo "404 Not Found";
+        return false;
+    }
+}
+
+function mk_session_id()
+{
+    $user = $_POST["name"];
+    $date = date("Y-m-d H:I:s");
+    $remote_ip = $_SERVER["REMOTE_ADDR"];
+    $seed = $user . $date . $remote_ip;
+    //echo ($seed);
+    return md5($seed);
+}
+
+?>
--- www/play.php
+++ www/play.php
@@ -1,4 +1,35 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+﻿<?php
+$ip = $_SERVER["SERVER_NAME"];
+$id = $_GET["id"];
+
+$db = mysql_connect("localhost", "hhac", "iamharmless");
+if(!$db) {
+  die("Could not connect: " . mysql_error());
+}
+$dbname = "hhac";
+mysql_select_db($dbname, $db) or die("Cannot switch to $dbname");
+
+$query_flv = "select * from videos where id=$id";
+$res = mysql_query($query_flv, $db) or die("Query error" . mysql_error());
+$rowcnt = mysql_num_rows($res);
+if($rowcnt == 0) {
+  die("<h1>404 Not Found</h1>");
+}
+$movie = mysql_fetch_array($res, MYSQL_ASSOC);
+$title = $movie["title"];
+$tags = $movie["tags"];
+$description = $movie["description"];
+$owner = $movie["owner"];
+
+$query_user = "select name from users where id='$owner'";
+$res_user = mysql_query($query_user, $db) or die("Query error" . mysql_error());
+$user = mysql_fetch_array($res_user, MYSQL_ASSOC);
+$user_name = $user["name"];
+
+mysql_close($db);
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
@@ -21,34 +52,6 @@
 }
   </style>
 </head>
-
-<?php
-$ip = $_SERVER["SERVER_NAME"];
-$id = $_GET["id"];
-
-$db = mysql_connect("localhost", "hhac", "iamharmless");
-if(!$db) {
-  die("Could not connect: " . mysql_error());
-}
-$dbname = "hhac";
-mysql_select_db($dbname, $db) or die("Cannot switch to $dbname");
-
-$query_flv = "select * from videos where id=$id";
-$res = mysql_query($query_flv, $db) or die("Query error" . mysql_error());
-$movie = mysql_fetch_array($res, MYSQL_ASSOC);
-$title = $movie["title"];
-$tags = $movie["tags"];
-$description = $movie["description"];
-$owner = $movie["owner"];
-
-$query_user = "select name from users where id='$owner'";
-$res_user = mysql_query($query_user, $db) or die("Query error" . mysql_error());
-$user = mysql_fetch_array($res_user, MYSQL_ASSOC);
-$user_name = $user["name"];
-
-mysql_close($db);
-?>
-
 
 <body>
     <div id="play_and_info">
